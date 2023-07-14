1758cb55f1f924f480bcc0bf55e11d8a604b5119
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-14 16:21:05 -0500

    Feat: traducir texto usando servicio de traducción

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from starlette.responses import RedirectResponse
+
+from translator_service import translate_text
 
 app = FastAPI()
 
@@ -7,3 +9,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return RedirectResponse("/docs", status_code=303)
+
+
+@app.post("/api/translate")
+def translate(
+        message: str = Form(...),
+        original_language: str = Form(...),
+        final_language: str = Form(...)
+):
+    translated_text = translate_text(
+        message_to_translate=message, base_language=original_language, language_to_translate=final_language
+    )
+
+    return translated_text
